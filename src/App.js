d0dcfd1b578cf01ad0db601646753a28a4c503c2
chaski123 <juanjesusperezcastillo@gmail.com>
2021-12-15 17:51:44 -0300

    Pimer guardado

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const claseObjeto = {
+    primary: "text-primary",
+    danger: "text-danger"
+  }
+
+  const user = false;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className={claseObjeto.primary}></p>
+          {
+            user ? "en linea" : "offline"
+          }
         <a
           className="App-link"
           href="https://reactjs.org"
